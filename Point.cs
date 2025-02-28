d635bdb5370c6b16b76d3111bcbd3380b28181a9
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-28 12:42:08 +0200

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_2
+{
+    internal class Point
+    {
+        /// <summary>
+        /// Поле Координата х
+        /// </summary>
+        double x;
+        /// <summary>
+        /// Поле Координата у
+        /// </summary>
+        double y;
+
+        /// <summary>
+        /// Статическое поле Кол-во точек.
+        /// </summary>
+        static int count;
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Point()
+        {
+            x = 1.0;
+            x = 1.0;
+            count++;
+        }
+
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="x">Координата х</param>
+        /// <param name="y">Координата у</param>
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            count++;
+        }
+
+        /// <summary>
+        /// Статический конструктор
+        /// </summary>
+        static Point()
+        {
+            count = 0;
+        }
+
+        /// <summary>
+        /// Метод для получения координаты х
+        /// </summary>
+        /// <returns>Координата х</returns>
+        public double GetX() { return x; }
+
+        /// <summary>
+        /// Метод для получения координаты у
+        /// </summary>
+        /// <returns>Координата у</returns>
+        public double GetY() { return y; }
+
+        /// <summary>
+        /// Метод для уставновления значения координаты х
+        /// </summary>
+        /// <param name="x"></param>
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+
+        /// <summary>
+        /// Метод для уставновления значения координаты у
+        /// </summary>
+        /// <param name="y"></param>
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Метод для вывода внутренних полей класса
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"x = {GetX()}\ty = {GetY()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(1.5, 2.5);
+            a.Print();
+            a.SetX(1);
+            Console.WriteLine("Обновил значениие x");
+            a.Print();
+            Console.WriteLine();
+
+            Point b = new Point(-3.8, -4.2);
+            b.Print();
+            b.SetY(0);
+            Console.WriteLine("Обновил значениие y");
+            b.Print();
+            Console.WriteLine();
+
+            Point c = new Point(0.1, 9.9);
+            c.Print();
+            double temp = c.GetX();
+            c.SetX(c.GetY());
+            c.SetY(temp);
+            Console.WriteLine("Поменял местами значение х и у");
+            c.Print();
+
+            Console.ReadKey(true);
+        }
+    }
+}
