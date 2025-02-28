a857506c957421205192efa1f1aac2595690daea
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-28 12:19:05 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26
+{
+    internal class Person
+    {
+        int age;
+
+        static int retirementAge = 60;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static int RetirementAge
+        {
+            get { return retirementAge; }
+            set
+            {
+                if(value > 100)
+                {
+                    retirementAge = 100;
+                }
+                else if(value < 1)
+                {
+                    retirementAge = 1;
+                }
+                else
+                {
+                    retirementAge = value;
+                }
+            }
+        }
+
+        public Person() { }
+
+        public Person(int age)
+        {
+            Age = age;
+        }
+
+        public void CheckAge()
+        {
+            if(Age >= retirementAge)
+            {
+                Console.WriteLine("Уже на пенсии");
+            }
+            else
+            {
+                Console.WriteLine($"До пенсии осталось {retirementAge - Age}");
+            }
+        }
+
+        public static void CheckRetirementStatus(Person person)
+        {
+            if (person.Age >= retirementAge)
+            {
+                Console.WriteLine("Уже на пенсии");
+            }
+            else
+            {
+                Console.WriteLine($"До пенсии осталось {retirementAge - person.Age} года(лет)");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Возраст: {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(90);
+            Console.WriteLine(person1);
+            person1.CheckAge();
+            Console.WriteLine();
+
+            Person person2 = new Person(18);
+            Console.WriteLine(person2);
+            Person.CheckRetirementStatus(person2);
+
+            Console.ReadKey(true);
+        }
+    }
+}
